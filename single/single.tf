ff97b456911eda811e749b94d70b0ac61489d1f6
Yimeng Chen <chenyimeng@jd.com>
2018-06-05 17:14:01 +0800

    add single tf file

--- single/single.tf
+++ single/single.tf
@@ -1,6 +1,6 @@
-resource "qingcloud_eip" "vpc"{
-  name = "eip-test"
-  description = "eip-test"
+resource "qingcloud_eip" "single"{
+  name = "eip-single"
+  description = "eip-single"
   billing_mode = "traffic"
   bandwidth = 1
   need_icp = 0
@@ -52,64 +52,19 @@ resource "qingcloud_keypair" "arthur"{
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
-resource "qingcloud_vpc" "vpc"{
-  name = "vpc-network"
-  type = 1
-  vpc_network = "172.21.0.0/16"
-  security_group_id = "${qingcloud_security_group.basic.id}"
-  description = "测试的网络"
-  eip_id = "${qingcloud_eip.vpc.id}"
-}
 
-resource "qingcloud_vpc_static" "web"{
-  vpc_id = "${qingcloud_vpc.vpc.id}"
-  type = 1
-  val1 = "80"
-  val2 = "${qingcloud_instance.master.private_ip}"
-  val3 = "80"
-}
-
-resource "qingcloud_vpc_static" "ssh"{
-  vpc_id = "${qingcloud_vpc.vpc.id}"
-  type = 1
-  val1 = "443"
-  val2 = "${qingcloud_instance.master.private_ip}"
-  val3 = "22"
-}
-
-resource "qingcloud_vxnet" "vx"{
-  name = "app vxnet"
-  type = 1
-  description = "应用的网络"
-  vpc_id = "${qingcloud_vpc.vpc.id}"
-  ip_network = "172.21.1.0/24"
-}
 
 resource "qingcloud_instance" "master"{
   image_id = "centos74x64"
   instance_class = "0"
-  managed_vxnet_id = "${qingcloud_vxnet.vx.id}"
   keypair_ids = ["${qingcloud_keypair.arthur.id}"]
   security_group_id ="${qingcloud_security_group.basic.id}"
+  eip_id = "${qingcloud_eip.single.id}"
 }
 
-resource "qingcloud_instance" "slave"{
-  count = 2
-
-  name = "slave-${count.index}"
-  image_id = "centos74x64"
-  instance_class = "0"
-  managed_vxnet_id = "${qingcloud_vxnet.vx.id}"
-  keypair_ids = ["${qingcloud_keypair.arthur.id}"]
-  security_group_id ="${qingcloud_security_group.basic.id}"
+output "ssh-shell" {
+  value = "ssh root@${qingcloud_eip.single.addr}"
 }
 
-output "ssh-vpc" {
-  value = "sshuttle  -r root@${qingcloud_eip.vpc.addr}:${qingcloud_vpc_static.ssh.val1} ${qingcloud_vxnet.vx.ip_network}"
-}
-
-  output "ssh-list" {
-    value = ["${qingcloud_instance.slave.*.private_ip}"]
-  }
 
 
